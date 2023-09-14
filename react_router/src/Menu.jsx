7b8dc824a222f27990bb3e03735d3de0933d4177
Ankit Mittal <ankitmittal0814@gmail.com>
2023-09-14 23:26:26 +0530

    Update menu.jsx

--- react_router/src/Menu.jsx
+++ react_router/src/Menu.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const Menu = () => {
   return (
     <>
-      <NavLink exact to="/" activeclassname="active_class">
+      <NavLink exact="true" to="/" activeclassname="active_class">
         About Us
       </NavLink>
-      <NavLink exact to="/contact" activeclassname="active_class">
+      <NavLink exact="true" to="/contact" activeclassname="active_class">
         Contact Us
       </NavLink>
       <br></br>
