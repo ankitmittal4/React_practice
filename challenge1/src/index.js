3525bb7f44e9fe964e78b359f7774dc5e3bc7d99
Ankit Mittal <ankitmittal0814@gmail.com>
2023-09-24 19:51:11 +0530

    Adding bootstrap

--- challenge1/src/index.js
+++ challenge1/src/index.js
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const todayDate = new Date().toLocaleDateString();
 const todayTime = new Date().toLocaleTimeString();
 
-
 let currDate = new Date();
 let year = currDate.getFullYear();
-let month = currDate.getMonth()+1;
+let month = currDate.getMonth() + 1;
 let date = currDate.getDate();
 const fullDate = `${date} - ${month} - ${year}`;
 
@@ -21,7 +21,6 @@ const img2 = "https://picsum.photos/250/300";
 const img3 = "https://picsum.photos/250/301";
 const link = "https://www.youtube.com/";
 ReactDOM.render(
-
   //Challenfe 1
   // <>
   //   <h1>Ankit Mittal Netflix Pick</h1>
@@ -37,15 +36,23 @@ ReactDOM.render(
 
   //challenge 2
   <>
-    <h1 contentEditable>My name is Ankit Mittal</h1>
-    <img src={img1} alt="Image"/>
-    <img src={img2} alt="Image"/>
+    <div>
+      <h1 contentEditable className="text-center">
+        My name is Ankit Mittal
+      </h1>
+      <br></br>
+      <div className="text-center ">
+        <button className="btn btn-outline-primary m-4">Submit</button>
+      </div>
+    </div>
+    <img src={img1} alt="Image" />
+    <img src={img2} alt="Image" />
     <a href={link} target="_blank">
-      <img src={img3} alt="Image"/>
+      <img src={img3} alt="Image" />
     </a>
     <h1>Today's Date is : {todayDate}</h1>
     <h1>Current Time is : {todayTime}</h1>
-
   </>,
 
-  document.getElementById("root"));
+  document.getElementById("root")
+);
